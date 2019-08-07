39f23b9029c8f77542f099741808c440c029e28b
to4kawa <to.4kawa@gmail.com>
2019-08-07 13:40:24 +0900

    plot2.R complate

--- plot2.R
+++ plot2.R
@@ -1,16 +1,22 @@
-library(dplyr)
 library(readr)
 library(lubridate)
 
-HPC_df <- read_delim("household_power_consumption.txt",delim=";",na=c("?","NA"))
 
-w1_df <- HPC_df %>% filter(Date %in% c("2/2/2007","1/2/2007"))
+# download file and unzip
+file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(file_url,"data.zip",method="curl")
+df <- {unzip("data.zip")}
 
-w1_weekday_df <- mutate(w1_df, weekDate = paste(dmy(w1_df$Date) ,w1_df$Time)) %>%
-  select(weekDate,Global_active_power)
+# read require cols to dataframe
+data_df <- read_delim(df,delim=";",na=c("?","NA"), col_types = "ccd______")
+plot2_df <- subset(data_df,Date %in% c("2/2/2007","1/2/2007"))
 
-png("plot2.png")
+# Date and Time cols change POSIXlt dataframe
+plot2_df_x <- strptime(paste(dmy(plot2_df$Date) ,plot2_df$Time),"%Y-%m-%d %H:%M:%S")
 
-with(w1_weekday_df, plot(strptime(weekDate,"%Y-%m-%d %H:%M:%S"),Global_active_power,type="l",xlab="",ylab="Global Active Power(kilowatts)"))
+# create png image
+png("plot2.png", width = 480, height = 480)
+
+plot(plot2_df_x, plot2_df$Global_active_power,type="l",xlab="",ylab="Global Active Power(kilowatts)")
 
 dev.off()
