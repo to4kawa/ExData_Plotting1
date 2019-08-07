e2645cc3016b41ed4f486c8f71c4853deb215322
to4kawa <to.4kawa@gmail.com>
2019-08-07 14:00:41 +0900

    plot3.R complate

--- plot3.R
+++ plot3.R
@@ -1,23 +1,29 @@
-library(dplyr)
 library(readr)
 library(lubridate)
 
-HPC_df <- read_delim("household_power_consumption.txt",delim=";",na=c("?","NA"))
 
-w1_df <- HPC_df %>% filter(Date %in% c("2/2/2007","1/2/2007"))
+# download file and unzip
+file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(file_url,"data.zip",method="curl")
+df <- {unzip("data.zip")}
 
-w1_weekday_df <- mutate(w1_df, weekDate = paste(dmy(w1_df$Date) ,w1_df$Time)) %>%
-  select(weekDate,starts_with("Sub"))
+# read require cols to dataframe
+data_df <- read_delim(df,delim=";",na=c("?","NA"), col_types = "cc____ddd")
+plot3_df <- subset(data_df,Date %in% c("2/2/2007","1/2/2007"))
 
-labels <- grep("Sub",names(w1_weekday_df),value=T)
+# Date and Time cols change POSIXlt dataframe
+plot3_df_x <- strptime(paste(dmy(plot3_df$Date) ,plot2_df$Time),"%Y-%m-%d %H:%M:%S")
 
+labels <- grep("Sub",names(plot3_df),value=T)
 
-with(w1_weekday_df, plot(strptime(weekDate,"%Y-%m-%d %H:%M:%S"),Sub_metering_1,type="l",ylim=c(0,40),xlab="",ylab=""))
+# create png image
+png("plot3.png", width = 480, height = 480)
+
+plot(plot3_df_x, plot3_df$Sub_metering_1,type="l", col="black", xlab="", ylab="", ylim=c(0,38))
 par(new=TRUE)
-with(w1_weekday_df, plot(strptime(weekDate,"%Y-%m-%d %H:%M:%S"),Sub_metering_2,type="l",ylim=c(0,40),xlab="",ylab="",col="red"))
+plot(plot3_df_x, plot3_df$Sub_metering_2, type="l", col="red", xlab="", ylab="", ylim=c(0,38))
 par(new=TRUE)
-with(w1_weekday_df, plot(strptime(weekDate,"%Y-%m-%d %H:%M:%S"),Sub_metering_3,type="l",ylim=c(0,40),xlab="",ylab="Energy sub metering",col="blue"))
-
+plot(plot3_df_x, plot3_df$Sub_metering_3, type="l", col="blue", xlab="", ylab="Energy sub metering", ylim=c(0,38))
 legend("topright", legend = labels, col = c("black","red","blue"), lty = c(1,1,1))
 
 dev.off()
